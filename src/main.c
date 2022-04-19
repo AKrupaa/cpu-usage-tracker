675a1375b13db121bf73fe7627695420b934021b
Arkadiusz <arkadiusz.krupinski15@gmail.com>
2022-04-19 11:15:01 +0200

    init

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const* argv[]) {
+  /* unused parameter */
+  (void)argc;
+  (void)argv;
+
+  printf("Hello World!\n");
+
+  return 0;
+}
